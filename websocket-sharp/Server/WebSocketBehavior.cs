b88614adead8fa6c4476f27b217b7ca1ab350dea
sta <sta.blockhead@gmail.com>
2025-10-17 22:20:11 +0900

    [Modify] Replace it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -989,7 +989,7 @@ namespace WebSocketSharp.Server
     /// </exception>
     protected void CloseAsync (ushort code, string reason)
     {
-      if (_websocket == null) {
+      if (!_registered) {
         var msg = "The session has not started yet.";
 
         throw new InvalidOperationException (msg);
