e1a57ca59e75a5b6bfb50ba16aed01b3ab8dab33
sta <sta.blockhead@gmail.com>
2026-01-18 17:04:28 +0900

    [Modify] Edit it

--- websocket-sharp/Net/WebHeaderCollection.cs
+++ websocket-sharp/Net/WebHeaderCollection.cs
@@ -1317,7 +1317,8 @@ namespace WebSocketSharp.Net
     /// characters.
     /// </exception>
     /// <exception cref="InvalidOperationException">
-    /// This instance does not allow the header.
+    /// This method is not available if the current instance does not allow
+    /// that header type.
     /// </exception>
     public override void Add (string name, string value)
     {
