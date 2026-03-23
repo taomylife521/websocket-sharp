5bd51b5be43dd9f5c22779bdaab1570f23db4a49
sta <sta.blockhead@gmail.com>
2026-03-23 16:35:51 +0900

    [Modify] Edit it

--- websocket-sharp/Net/WebHeaderCollection.cs
+++ websocket-sharp/Net/WebHeaderCollection.cs
@@ -1580,7 +1580,7 @@ namespace WebSocketSharp.Net
     /// <c>false</c>.
     /// </returns>
     /// <param name="headerName">
-    /// A <see cref="string"/> that specifies the name of the header to test.
+    /// A <see cref="string"/> that specifies the name of a header to test.
     /// </param>
     /// <param name="response">
     /// A <see cref="bool"/>: <c>true</c> if the test is for the response;
